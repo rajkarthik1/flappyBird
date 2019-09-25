37dcc66a0e43349f96b64460d0e597fd27ba6269
KarthikRaj <rajkarthik711@gmail.com>
2019-09-25 11:11:45 -0400

    adding player image and enemy image

---
+++ app/src/main/java/com/example/tappyspaceship01/GameEngine.java
@@ -0,0 +1,193 @@
+package com.example.tappyspaceship01;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.Random;
+
+public class GameEngine extends SurfaceView implements Runnable {
+
+    // Android debug variables
+    final static String TAG="TAPPY-SPACESHIP";
+
+    // screen size
+    int screenHeight;
+    int screenWidth;
+
+    // game state
+    boolean gameIsRunning;
+
+    // threading
+    Thread gameThread;
+
+
+    // drawing variables
+    SurfaceHolder holder;
+    Canvas canvas;
+    Paint paintbrush;
+
+
+
+    // -----------------------------------
+    // GAME SPECIFIC VARIABLES
+    // -----------------------------------
+
+    // ----------------------------
+    // ## SPRITES
+    // ----------------------------
+
+
+    // ----------------------------
+    // ## GAME STATS
+    // ----------------------------
+
+    public GameEngine(Context context, int w, int h) {
+        super(context);
+
+
+        this.holder = this.getHolder();
+        this.paintbrush = new Paint();
+
+        this.screenWidth = w;
+        this.screenHeight = h;
+
+
+        this.printScreenInfo();
+
+        // @TODO: Add your sprites
+        // @TODO: Any other game setup
+
+    }
+
+
+    private void printScreenInfo() {
+
+        Log.d(TAG, "Screen (w, h) = " + this.screenWidth + "," + this.screenHeight);
+    }
+
+    private void spawnPlayer() {
+        //@TODO: Start the player at the left side of screen
+    }
+    private void spawnEnemyShips() {
+        Random random = new Random();
+
+        //@TODO: Place the enemies in a random location
+
+    }
+
+    // ------------------------------
+    // GAME STATE FUNCTIONS (run, stop, start)
+    // ------------------------------
+    @Override
+    public void run() {
+        while (gameIsRunning == true) {
+            this.updatePositions();
+            this.redrawSprites();
+            this.setFPS();
+        }
+    }
+
+
+    public void pauseGame() {
+        gameIsRunning = false;
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {
+            // Error
+        }
+    }
+
+    public void startGame() {
+        gameIsRunning = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+    // ------------------------------
+    // GAME ENGINE FUNCTIONS
+    // - update, draw, setFPS
+    // ------------------------------
+
+
+
+    public void updatePositions() {
+        // @TODO: Update position of player
+
+
+
+    }
+
+    public void redrawSprites() {
+        if (this.holder.getSurface().isValid()) {
+            this.canvas = this.holder.lockCanvas();
+
+            //----------------
+
+            // configure the drawing tools
+            this.canvas.drawColor(Color.argb(255,255,255,255));
+            paintbrush.setColor(Color.WHITE);
+
+
+            // DRAW THE PLAYER HITBOX
+            // ------------------------
+            // 1. change the paintbrush settings so we can see the hitbox
+            paintbrush.setColor(Color.BLUE);
+            paintbrush.setStyle(Paint.Style.STROKE);
+            paintbrush.setStrokeWidth(5);
+        // draw player graphic on the screen
+            Bitmap playerImage = BitmapFactory.decodeResource(this.getContext().getResources(),
+                    R.drawable.player_ship);
+
+            canvas.drawBitmap(playerImage, 100, 120, paintbrush);
+
+            // draw enemy graphic on the screen
+
+            Bitmap enemyImage = BitmapFactory.decodeResource(this.getContext().getResources(),
+                    R.drawable.alien_ship2);
+
+            canvas.drawBitmap(enemyImage, 1300, 120, paintbrush);
+
+            //----------------
+            this.holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void setFPS() {
+        try {
+            gameThread.sleep(120);
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+    // ------------------------------
+    // USER INPUT FUNCTIONS
+    // ------------------------------
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int userAction = event.getActionMasked();
+        //@TODO: What should happen when person touches the screen?
+        if (userAction == MotionEvent.ACTION_DOWN) {
+            Log.d(TAG, "Person tapped the screen");
+
+
+        }
+        else if (userAction == MotionEvent.ACTION_UP) {
+            Log.d(TAG, "Person lifted finger");
+        }
+
+        return true;
+    }
+}
